ec2911679f2ecb06375bb2fe120c52b4fb6558b3
Ayush <ayushmahobia01@gmail.com>
2023-06-16 12:55:40 +0530

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
       <div className='sticky-top'>
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">News Hub</Link>
+            <Link className="navbar-brand" to="/">ChronoScope</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
